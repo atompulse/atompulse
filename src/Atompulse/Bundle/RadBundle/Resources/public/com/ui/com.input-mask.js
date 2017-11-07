f954878b868235f28df2e0f411449694c391addd
petru <petru.cojocar@gmail.com>
2017-11-07 18:05:59 +0200

    - resolved conflict with input[number] vs input[text]

--- src/Atompulse/Bundle/RadBundle/Resources/public/com/ui/com.input-mask.js
+++ src/Atompulse/Bundle/RadBundle/Resources/public/com/ui/com.input-mask.js
@@ -11,18 +11,25 @@ angular.module('Web.Components')
             return {
                 restrict: 'A',
                 require: 'ngModel',
-                link: function (scope, el, attrs, ngModel) {
-
-                    $(el).inputmask(scope.$eval(attrs.inputMask));
-
-                    ngModel.$parsers.push(function(value) {
+                link: function (scope, el, attrs, ngModel)
+                {
+                    // first handle angular model
+                    ngModel.$parsers.push(function(value)
+                    {
                         var parsedValue = parseFloat(value);
+
                         if (_.isNumber(parsedValue)) {
                             return parsedValue;
                         }
+
                         return value;
                     });
 
+                    // next do the input-mask binding
+                    if (el.prop('type') !== 'text') {
+                        el.prop('type', 'text');
+                    }
+                    $(el).inputmask(scope.$eval(attrs.inputMask));
                 }
             };
         }
